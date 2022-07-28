6e28ddc90b233a7fc6423c297081e191d9228e6c
Elane Garcia <elanegarcia@ciandt.com>
2022-07-28 15:27:45 -0300

    Task 0

---
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionSQLite {
+
+    private static final String URL = "jdbc:sqlite:sqlite_database_2022.db";
+
+    public static void main(String[] args) {
+        initConnection();
+    }
+
+    public static void initConnection() {
+        try(Connection ignored = DriverManager.getConnection(URL)) {
+            System.out.println("Conexão estabelecida.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}

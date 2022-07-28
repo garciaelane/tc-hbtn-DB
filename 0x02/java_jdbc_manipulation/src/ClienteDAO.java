176f6113c7be18063190b69d069b43bcb419772c
Elane Garcia <elanegarcia@ciandt.com>
2022-07-28 18:56:06 -0300

    Task 0

--- 0x02/java_jdbc_manipulation/src/ClienteDAO.java
+++ 0x02/java_jdbc_manipulation/src/ClienteDAO.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ClienteDAO {
+import java.sql.Connection;
+
+public interface ClienteDAO {
+
+    public Connection connect(String urlConexao);
+    public void createTable(String urlConexao);
+    public void insert(String url_conexao, Cliente cliente);
+    public void selectAll(String urlConexao);
+    public void update(String urlConexao, int id, String name, Integer idade);
+    public void delete(String urlConexao, int id);
+
 }
--- 0x02/java_jdbc_manipulation/src/ClienteDAOImpl.java
+++ 0x02/java_jdbc_manipulation/src/ClienteDAOImpl.java
@@ -1,2 +1,30 @@
-public class ClienteDAOImpl {
+public class ClienteDAOImpl implements ClienteDAO {
+    @Override
+    public void connect(String urlConexao) {
+    }
+
+    @Override
+    public void createTable(String urlConexao) {
+
+    }
+
+    @Override
+    public void insert(String url_conexao, Cliente cliente) {
+
+    }
+
+    @Override
+    public void selectAll(String urlConexao) {
+
+    }
+
+    @Override
+    public void update(String urlConexao, int id, String name, Integer idade) {
+
+    }
+
+    @Override
+    public void delete(String urlConexao, int id) {
+
+    }
 }

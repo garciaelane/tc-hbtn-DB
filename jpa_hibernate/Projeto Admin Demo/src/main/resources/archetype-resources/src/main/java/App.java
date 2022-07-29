09bf96aecf432d6ebe7605cfbfa601210d1c2fb7
Elane Garcia <elanegarcia@ciandt.com>
2022-07-28 22:33:05 -0300

    JPA e Hibernate

---
+++ jpa_hibernate/Projeto Admin Demo/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.techcamps.cadastros;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
